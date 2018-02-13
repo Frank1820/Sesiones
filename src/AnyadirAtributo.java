7298c1b1ef39d9af35f1dbd23a83071064a47063
frank1820 <franciscogonzalez_87@hotmail.com>
2018-02-13 16:12:35 +0100

    Servlet AnyadirAtributo creado

---
+++ src/AnyadirAtributo.java
@@ -0,0 +1,70 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class AnyadirAtributo
+ */
+@WebServlet("/anyadir")
+public class AnyadirAtributo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AnyadirAtributo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//Recuperamos los dos atributos quen os manda el formulario
+		String atributo= request.getParameter("atributo");
+		String valor= request.getParameter("valor");
+		
+		//Recuperamos la sesión del usuario
+		HttpSession sesion =request.getSession();
+		
+		//Añadimos a la sesión la información del formulario guardada en el paso anterior.
+		sesion.setAttribute(atributo, valor);
+		
+		//Salida del servlet para el usuario
+		response.setContentType("text/html");
+		
+		PrintWriter out=response.getWriter();
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Esto es un servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hola, Mundo</h1>");
+		out.println("<p>Esto es una respuesta recibida desde un servlet.</p>");
+		out.println("</body>");
+		out.println("</html>");
+		
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
